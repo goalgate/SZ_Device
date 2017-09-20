f0ac7ffe3e3a0fe49cbcf6b5e55348c110a20002
goalgate <522508134@qq.com>
2017-09-20 11:17:45 +0800

    接口

---
+++ app/src/main/java/com/sz_device/Tools/MyObserver.java
@@ -0,0 +1,36 @@
+package com.sz_device.Tools;
+
+
+import com.sz_device.Retrofit.Response.ResponseEnvelope;
+
+import butterknife.OnClick;
+import io.reactivex.Observer;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+
+/**
+ * Created by zbsz on 2017/9/19.
+ */
+
+public class MyObserver implements Observer<ResponseEnvelope> {
+
+    @Override
+    public void onComplete() {
+
+    }
+
+    @Override
+    public void onError(@NonNull Throwable e) {
+
+    }
+
+    @Override
+    public void onNext(@NonNull ResponseEnvelope responseEnvelope) {
+
+    }
+
+    @Override
+    public void onSubscribe(@NonNull Disposable d) {
+
+    }
+}
