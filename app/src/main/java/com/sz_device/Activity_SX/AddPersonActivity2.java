1708d30185cfe25503f7925be5198441c7df2a76
goalgate <522508134@qq.com>
2019-09-29 18:52:58 +0800

    绍兴采集器

--- app/src/main/java/com/sz_device/Activity_SX/AddPersonActivity2.java
+++ app/src/main/java/com/sz_device/Activity_SX/AddPersonActivity2.java
@@ -1,11 +1,10 @@
-package com.sz_device.TestActivity;
+package com.sz_device.Activity_SX;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,16 +14,20 @@ import com.blankj.utilcode.util.BarUtils;
 import com.blankj.utilcode.util.SPUtils;
 import com.blankj.utilcode.util.TimeUtils;
 import com.blankj.utilcode.util.ToastUtils;
+import com.drv.card.ICardInfo;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.jakewharton.rxbinding2.view.RxView;
 import com.log.Lg;
 import com.sz_device.Alerts.Alarm;
 import com.sz_device.AppInit;
+import com.sz_device.Bean.FingerprintUser;
 import com.sz_device.Bean.ReUploadBean;
 import com.sz_device.EventBus.OpenDoorEvent;
 import com.sz_device.Function.Fun_FingerPrint.mvp.presenter.FingerPrintPresenter;
 import com.sz_device.Function.Fun_FingerPrint.mvp.view.IFingerPrintView;
+import com.sz_device.Function.Func_ICCard.mvp.presenter.IDCardPresenter;
+import com.sz_device.Function.Func_ICCard.mvp.view.IIDCardView;
 import com.sz_device.R;
 import com.sz_device.Retrofit.RetrofitGenerator;
 import com.sz_device.Tools.FileUtils;
@@ -53,9 +56,9 @@ import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
 import okhttp3.ResponseBody;
 
-public class AddPersonActivity_copy extends Activity implements IFingerPrintView {
+public class AddPersonActivity2 extends Activity implements IFingerPrintView, IIDCardView {
 
-    private String TAG = AddPersonActivity_copy.class.getSimpleName();
+    private String TAG = AddPersonActivity2.class.getSimpleName();
 
     SPUtils config = SPUtils.getInstance("config");
 
@@ -72,21 +75,24 @@ public class AddPersonActivity_copy extends Activity implements IFingerPrintView
     @BindView(R.id.iv_finger)
     ImageView img_finger;
 
-    @BindView(R.id.et_finger)
+    @BindView(R.id.tv_finger)
     TextView tv_finger;
 
     @BindView(R.id.btn_commit)
     Button btn_commit;
 
-    @BindView(R.id.et_idcard)
-    EditText et_idcard;
+    @BindView(R.id.tv_person_name)
+    TextView tv_person_name;
 
-    @BindView(R.id.btn_query)
-    Button query;
+    @BindView(R.id.tv_id_card)
+    TextView tv_id_card;
 
-    @OnClick(R.id.btn_query)
-    void queryPerson() {
-        RetrofitGenerator.getCommonApi().queryPersonInfo("queryPersonInfo", config.getString("key"), et_idcard.getText().toString().toUpperCase())
+    @BindView(R.id.iv_head_photo)
+    ImageView iv_head_photo;
+
+
+    void queryPerson(final ICardInfo cardInfo) {
+        RetrofitGenerator.getShaoXingApi().queryPersonInfo("queryPersonInfo", config.getString("key"), cardInfo.cardId())
                 .subscribeOn(Schedulers.io())
                 .unsubscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
@@ -99,24 +105,25 @@ public class AddPersonActivity_copy extends Activity implements IFingerPrintView
                                     }.getType());
                             if (infoMap.get("result").equals("true")) {
                                 if (infoMap.get("status").equals(String.valueOf(0))) {
-                                    fp_id = infoMap.get("data");
+                                    fp_id = String.valueOf(fpp.fpGetEmptyID());
                                     img_finger.setClickable(false);
                                     fpp.fpEnroll(fp_id);
                                     user = new User();
-                                    user.setCardId(et_idcard.getText().toString().toUpperCase());
+                                    user.setCardId(cardInfo.cardId());
                                     user.setName(infoMap.get("name"));
                                     user.setFingerprintId(fp_id);
                                     user.setCourIds(infoMap.get("courIds"));
                                     user.setCourType(infoMap.get("courType"));
-                                    query.setText(infoMap.get("name") + ",欢迎您！");
-                                    query.setClickable(false);
+
+                                    AppInit.getInstrumentConfig().stopReadCard();
+
                                 } else {
-                                    Alarm.getInstance(AddPersonActivity_copy.this).messageAlarm("您的身份有误，如有疑问请联系客服处理");
+                                    Alarm.getInstance(AddPersonActivity2.this).messageAlarm("您的身份有误，如有疑问请联系客服处理");
                                 }
                             } else if (infoMap.get("result").equals("false")) {
-                                Alarm.getInstance(AddPersonActivity_copy.this).messageAlarm("系统未能查询到该人员信息，如有疑问请联系客服处理");
+                                Alarm.getInstance(AddPersonActivity2.this).messageAlarm("系统未能查询到该人员信息，如有疑问请联系客服处理");
                             } else {
-                                Alarm.getInstance(AddPersonActivity_copy.this).messageAlarm(infoMap.get("result"));
+                                Alarm.getInstance(AddPersonActivity2.this).messageAlarm(infoMap.get("result"));
                             }
                         } catch (IOException e) {
                             Lg.e(TAG,e.toString());
@@ -145,25 +152,39 @@ public class AddPersonActivity_copy extends Activity implements IFingerPrintView
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-                RetrofitGenerator.getCommonApi().withDataRs("fingerLog", config.getString("key"), jsonObject.toString())
+                RetrofitGenerator.getShaoXingApi().withDataRs("fingerLog", config.getString("key"), jsonObject.toString())
                         .subscribeOn(Schedulers.io())
                         .unsubscribeOn(Schedulers.io())
                         .observeOn(AndroidSchedulers.mainThread())
                         .subscribe(new MyObserver<String>(this) {
                             @Override
                             public void onNext(String s) {
-                                try{
+                                try {
                                     if (s.equals("true")) {
-                                        SPUtils user_sp = SPUtils.getInstance(user.getFingerprintId());
-                                        user_sp.put("courIds", user.getCourIds());
-                                        user_sp.put("name", user.getName());
-                                        user_sp.put("cardId", user.getCardId());
-                                        user_sp.put("courType", user.getCourType());
+                                        FingerprintUser fingerprintUser = new FingerprintUser();
+                                        fingerprintUser.setCourIds(user.getCourIds());
+                                        fingerprintUser.setCardId(user.getCardId());
+                                        fingerprintUser.setName(user.getName());
+                                        fingerprintUser.setCourType(user.getCourType());
+                                        fingerprintUser.setFingerprintId(user.getFingerprintId());
+                                        fingerprintUser.setFingerprintPhoto(user.getFingerprintPhoto());
+                                        fingerprintUser.setFingerprintKey(fpp.fpUpTemlate(user.getFingerprintId()));
+                                        mdaoSession.insertOrReplace(fingerprintUser);
+//                                        SPUtils user_sp = SPUtils.getInstance(user.getFingerprintId());
+//                                        user_sp.put("courIds", user.getCourIds());
+//                                        user_sp.put("name", user.getName());
+//                                        user_sp.put("cardId", user.getCardId());
+//                                        user_sp.put("courType", user.getCourType());
+//                                        SPUtils user_id = SPUtils.getInstance(user.getCardId());
+//                                        user_id.put("courIds", user.getCourIds());
+//                                        user_id.put("name", user.getName());
+//                                        user_id.put("fingerprintId", user.getFingerprintId());
+//                                        user_id.put("courType", user.getCourType());
                                         fp_id = "0";
                                         ToastUtils.showLong("人员插入成功");
                                         cancel();
                                     } else {
-                                        Alarm.getInstance(AddPersonActivity_copy.this).messageAlarm("数据插入有错");
+                                        Alarm.getInstance(AddPersonActivity2.this).messageAlarm("数据插入有错");
                                     }
                                 }catch (Exception e){
                                     Lg.e(TAG,e.toString());
@@ -171,30 +192,32 @@ public class AddPersonActivity_copy extends Activity implements IFingerPrintView
                             }
                         });
             } else {
-                Alarm.getInstance(AddPersonActivity_copy.this).messageAlarm("您的操作有误，请重试");
+                Alarm.getInstance(AddPersonActivity2.this).messageAlarm("您的操作有误，请重试");
 
             }
         } else {
-            Alarm.getInstance(AddPersonActivity_copy.this).messageAlarm("您还有信息未登记，如需退出请按取消");
+            Alarm.getInstance(AddPersonActivity2.this).messageAlarm("您还有信息未登记，如需退出请按取消");
         }
     }
 
     @OnClick(R.id.btn_cancel)
     void cancel() {
-        new AlertView("请选择接下来的操作", null, null, new String[]{"重置并继续录入指纹", "退出至主桌面"}, null, AddPersonActivity_copy.this, AlertView.Style.Alert, new OnItemClickListener() {
+        new AlertView("请选择接下来的操作", null, null, new String[]{"重置并继续录入指纹", "退出至主桌面"}, null, AddPersonActivity2.this, AlertView.Style.Alert, new OnItemClickListener() {
             @Override
             public void onItemClick(Object o, int position) {
                 if (position == 0) {
                     commitable = false;
-                    query.setClickable(true);
-                    query.setText("校验人员信息");
-                    et_idcard.setHint("请填写身份证信息");
-                    et_idcard.setText(null);
                     user = new User();
+                    AppInit.getInstrumentConfig().readCard();
+                    tv_id_card.setHint(R.string.cardHint);
+                    tv_id_card.setText(null);
+                    tv_person_name.setHint(R.string.nameHint);
+                    tv_person_name.setText(null);
+                    iv_head_photo.setImageBitmap(null);
                     img_finger.setClickable(false);
                     fpp.fpCancel(true);
                     fpp.fpRemoveTmpl(fp_id);
-                    tv_finger.setText("先验证人员身份获得指纹编号");
+                    tv_finger.setText("请刷身份证以获得指纹编号");
                     img_finger.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.zw_icon));
                 } else {
                     fpp.fpCancel(true);
@@ -210,7 +233,7 @@ public class AddPersonActivity_copy extends Activity implements IFingerPrintView
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         BarUtils.hideStatusBar(this);
-        setContentView(R.layout.activity_add_person);
+        setContentView(R.layout.activty_new_add_person);
         ButterKnife.bind(this);
         RxView.clicks(img_finger).throttleFirst(3, TimeUnit.SECONDS)
                 .observeOn(AndroidSchedulers.mainThread())
@@ -246,6 +269,9 @@ public class AddPersonActivity_copy extends Activity implements IFingerPrintView
     @Override
     protected void onResume() {
         super.onResume();
+        AppInit.getInstrumentConfig().readCard();
+        IDCardPresenter.getInstance().IDCardPresenterSetView(this);
+
         commitable = false;
         fpp.FingerPrintPresenterSetView(this);
 
@@ -254,6 +280,9 @@ public class AddPersonActivity_copy extends Activity implements IFingerPrintView
     @Override
     protected void onPause() {
         super.onPause();
+        AppInit.getInstrumentConfig().stopReadCard();
+        IDCardPresenter.getInstance().IDCardPresenterSetView(null);
+
         fpp.fpCancel(true);
         fpp.FingerPrintPresenterSetView(null);
     }
@@ -273,7 +302,7 @@ public class AddPersonActivity_copy extends Activity implements IFingerPrintView
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        RetrofitGenerator.getCommonApi().withDataRs("openDoorRecord", config.getString("key"), OpenDoorjson.toString())
+        RetrofitGenerator.getShaoXingApi().withDataRs("openDoorRecord", config.getString("key"), OpenDoorjson.toString())
                 .subscribeOn(Schedulers.io())
                 .unsubscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
@@ -305,6 +334,18 @@ public class AddPersonActivity_copy extends Activity implements IFingerPrintView
     @Override
     public void onFpSucc(String msg) {
 
+    }
+
+    @Override
+    public void onsetCardImg(Bitmap bmp) {
+        iv_head_photo.setImageBitmap(bmp);
+    }
+
+    @Override
+    public void onsetCardInfo(ICardInfo cardInfo) {
+        queryPerson(cardInfo);
+        tv_person_name.setText(cardInfo.name());
+        tv_id_card.setText(cardInfo.cardId());
     }
 
 }
